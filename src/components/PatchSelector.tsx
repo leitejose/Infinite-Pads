6dbf2b9c4be902098124701762cd482e6514cc35
José Neto <joseleite688@gmail.com>
2025-05-13 00:23:08 +0100

    First commit: projeto Infinite Pads

--- src/components/PatchSelector.tsx
+++ src/components/PatchSelector.tsx
@@ -9,7 +9,7 @@ export function PatchSelector({ patch, onChange }: { patch: string; onChange: (p
         {patches.map((p) => (
           <Button
             key={p}
-            variant={p === patch ? "default" : "line"}
+            variant={p === patch ? "default" : "null"}
             onClick={() => onChange(p)}
             className="text-2xl rounded-lg p-2 flex items-center justify-center w-full h-full bg-gradient-to-b from-[#eb214e] to-[#eb214e] hover:bg-gradient-to-b hover:from-[#f53e67] hover:to-[#f53e67] focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:pointer-events-none data-[state=open]:bg-slate-100 dark:bg-slate-800 dark:hover:bg-slate-700 dark:focus-visible:ring-slate-600 dark:data-[state=open]:bg-slate-100"
           >
